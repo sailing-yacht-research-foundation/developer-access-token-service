7ca891c8213d85d7ca77f0bb9ed3deb9f73b55e8
Aan Fadhil <ditemukan@gmail.com>
2021-07-28 23:35:24 +0700

    adjust tf to enable coexists with live data server

--- deployment/outputs.tf
+++ deployment/outputs.tf
@@ -5,21 +5,3 @@ output "ecr_repo_url" {
 output "alb_dns_name" {
   value = aws_alb.application_load_balancer.dns_name
 }
-
-output "rds_hostname" {
-  description = "RDS instance hostname"
-  value       = aws_db_instance.syrf.address
-  sensitive   = true
-}
-
-output "rds_port" {
-  description = "RDS instance port"
-  value       = aws_db_instance.syrf.port
-  sensitive   = true
-}
-
-output "rds_username" {
-  description = "RDS instance root username"
-  value       = aws_db_instance.syrf.username
-  sensitive   = true
-}
